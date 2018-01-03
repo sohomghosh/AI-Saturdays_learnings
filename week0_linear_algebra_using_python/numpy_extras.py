13b714fca370249c7568191448154a29f352988c
Sohom Ghosh <sohom1ghosh@gmail.com>
2018-01-03 20:05:53 +0530

    Create numpy_extras.py

---
+++ week0_linear_algebra_using_python/numpy_extras.py
@@ -0,0 +1,91 @@
+#add a border (filled with 0's) around an existing array
+np.pad
+
+#Other functions
+np.unravel_index
+np.tile
+np.dtype
+np.dot
+np.uniform
+np.copysign
+np.ceil
+np.abs
+np.intersect1d #find common values between two arrays
+np.seterr
+np.errstate
+np.datetime64
+np.timedelta64
+np.arange(dtype=datetime64['D'])
+np.add(out=), np.negative(out=), np.multiply(out=), np.divide(out=) #compute ((A+B)*(-A/2)) in place (without copy)
+np.arange
+np.fromiter
+np.linspace
+sort
+np.add.reduce #faster np.sum
+np.allclose #Check if 2 arrays are equal
+np.array_equal #Check if 2 arrays are equal
+flags.writeable #Make array immutable
+np.sqrt, np.arctan2 #Cartesian to polar coordinates
+argmax
+np.meshgrid
+np.subtract.outer #can be used to construct the Cauchy matrix C (Cij =1/(xi - yj))
+np.iinfo
+np.finfo
+eps
+np.set_printoptions
+argmin
+np.atleast_2d
+T
+np.sqrt
+astype(copy=False)
+np.genfromtxt
+np.ndenumerate
+np.ndindex
+np.meshgrid
+np.exp
+np.put
+np.random.choice
+mean(axis=,keepdims=)
+argsort
+any #check if 2d array as null columns
+~
+np.abs
+argmin
+flat
+np.nditer
+np.bincount
+np.add.at
+np.unique
+sum(axis=(-2,-1))
+np.bincount
+np.diag
+array[::4]
+array[:, :, None]
+array[[]] = array[[]]
+repeat
+np.roll
+np.sort
+view
+np.unique
+np.cumsum
+from numpy.lib import stride_tricks
+np.logical_not, np.negative
+stride_tricks.as_strided
+np.linalg.svd #compute matrix rank
+np.bincount
+argmax
+np.tensordot
+np.add.reduceat
+np.argsort
+np.argpartition
+np.indices
+np.core.records.fromarrays
+np.power
+np.einsum
+np.where
+np.unpackbits
+np.ascontiguousarray
+np.interp
+np.logical_and.reduce
+np.mod
+np.percentile
